eb58231043a9167162eacec215c73cb8182dc04d
khodary94 <khodary94@aucegypt.edu>
2015-07-06 18:35:44 +0200

    JSON Parser Added

--- app/src/main/java/com/Example/iJam/FirstFragment.java
+++ app/src/main/java/com/Example/iJam/FirstFragment.java
@@ -2,13 +2,19 @@ package com.Example.iJam;
 //import android.app.Fragment;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 
 public class FirstFragment extends Fragment {
@@ -17,9 +23,42 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_first_fragment, container, false);
 
+        ArrayList<Track> tracksList = new ArrayList<Track>();
+        String strJ = "[\n" +
+                "    {\n" +
+                "        \"Title\":\"Title1\",\n" +
+                "        \"Uploader\":\"Uploader1\",\n" +
+                "        \"Duration\":90,\n" +
+                "        \"Likes\": 120,\n" +
+                "        \"Rating\":3,\n" +
+                "        \"Instrument\":\"Violin\",\n" +
+                "        \"Tags\":\n" +
+                "        [\n" +
+                "            {\n" +
+                "                \"Tag0\":\"#Tag1\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"Tag1\":\"Tag2\"\n" +
+                "            }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "]";
+
+        try {
+            JSONArray j = new JSONArray(strJ);
+            tracksList = Track.parseJson(j);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ListView lvTracks = (ListView) v.findViewById(R.id.lvTracks);
+        myAdapter tracksAdap = new myAdapter(getActivity(), MainActivity.class, (ArrayList<myInterface>) (ArrayList<?>) tracksList);
+        lvTracks.setAdapter(tracksAdap);
+
         TextView tv = (TextView) v.findViewById(R.id.tvFragFirst);
         tv.setText(getArguments().getString("msg"));
         Button test=(Button)v.findViewById(R.id.button4);
+
         test.setOnClickListener(new View.OnClickListener() {
 
             @Override
