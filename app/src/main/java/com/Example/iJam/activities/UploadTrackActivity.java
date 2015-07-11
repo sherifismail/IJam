87c2d3e6a2caabdedfb271fb3e2d2fa4b0aa5c3f
Mostafa Ghoneimy <mghoneimy@yubb-software.com>
2015-07-12 01:51:57 +0200

    Main activity updated to match new database! image uploading to tracks is complete with all functionalities correct

--- app/src/main/java/com/Example/iJam/activities/UploadTrackActivity.java
+++ app/src/main/java/com/Example/iJam/activities/UploadTrackActivity.java
@@ -3,6 +3,7 @@ package com.Example.iJam.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.Example.iJam.R;
+import com.Example.iJam.network.InsertTrackTask;
 import com.Example.iJam.network.ServerManager;
 import com.Example.iJam.network.HttpImageTask;
 
@@ -25,6 +27,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class UploadTrackActivity extends AppCompatActivity implements View.OnClickListener{
@@ -95,24 +99,6 @@ public class UploadTrackActivity extends AppCompatActivity implements View.OnCli
                         Uri imageUri = data.getData();
                         final InputStream imageStream = getContentResolver().openInputStream(imageUri);
                         final Bitmap img = BitmapFactory.decodeStream(imageStream);
-
-                        new HttpImageTask(ServerManager.getServerURL() + "/tracks/upload_image.php", getApplicationContext()){
-                            @Override
-                            protected void onPostExecute(String s) {
-                                if(s.equals(""))
-                                    return;
-                                try {
-                                    JSONObject response = new JSONObject(s);
-                                    if (response.getString("status").equals("success")) {
-                                        Toast.makeText(ctx, response.getString("url"), Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        Toast.makeText(ctx, response.getString("error"), Toast.LENGTH_SHORT).show();
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }.execute(img);
                         imgTrack.setImageBitmap(img);
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -155,45 +141,71 @@ public class UploadTrackActivity extends AppCompatActivity implements View.OnCli
                 }
                 break;
             case R.id.trackupload_bt_upload:
-                /*
-                String name = etName.getText().toString().trim();
-                String instrument = etInstrument.getText().toString().trim();
-                String tags = etTags.getText().toString().trim();
-                JSONObject json_track = new JSONObject();
-                boolean val = false;
-                try {
-                    json_track.put("name", name);
-                    json_track.put("band", val);
-                    json_track.put("band_id", 4);
-                    json_track.put("user_id", MainActivity.user.getUser_id());
-                    json_track.put("instrument", instrument);
-                    json_track.put("duration", 24);
-                    json_track.put("upload_date", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
-                    json_track.put("tags", tags);
-                    json_track.put("img_url", "http:/ahourl");
-                    json_track.put("track_url", "http:/wahokamanwa7ed");
+                final String name = etName.getText().toString().trim();
+                final String instrument = etInstrument.getText().toString().trim();
+                final String tags = etTags.getText().toString().trim();
 
-                    new InsertTrackTask(getApplicationContext()) {
-                        @Override
-                        protected void onPostExecute(String s) {
-                            try {
-                                JSONObject response = new JSONObject(s);
-                                String status = response.getString("status");
-                                ServerManager.setServerStatus(status);
-                                if (status.equals("fail")) {
-                                    Toast.makeText(ctx, "Failed to upload track! " + response.getString("error"), Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(ctx, "Success", Toast.LENGTH_SHORT).show();
-                                    finish();
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                //----------------------------------------------------------------------------------------------------------------
+                //AFTER VALIDATION
+                //UPLOAD IMAGE
+
+                Bitmap img = ((BitmapDrawable)imgTrack.getDrawable()).getBitmap();
+
+                new HttpImageTask(ServerManager.getServerURL() + "/tracks/upload_image.php", getApplicationContext()){
+                    @Override
+                    protected void onPostExecute(String s) {
+                        String img_url = null;
+                        if(s.equals(""))
+                            return;
+                        try {
+                            JSONObject response = new JSONObject(s);
+                            if (response.getString("status").equals("success")) {
+                                img_url = ServerManager.getServerURL() + "/tracks/" + response.getString("url");
+                                Toast.makeText(ctx, img_url, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(ctx, response.getString("error"), Toast.LENGTH_SHORT).show();
                             }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    }.execute(json_track);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }*/
+
+                        //---------------------------------------------------------------------------------------------
+                        //UPLOAD TRACK TO DATABASE
+                        JSONObject json_track = new JSONObject();
+                        boolean val = false;
+                        try {
+                            json_track.put("name", name);
+                            json_track.put("user_name", MainActivity.user.getUser_name());
+                            json_track.put("band_name", JSONObject.NULL);
+                            json_track.put("duration", 24);
+                            json_track.put("upload_date", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+                            json_track.put("instrument", instrument);
+                            json_track.put("tags", tags);
+                            json_track.put("img_url", img_url);
+                            json_track.put("track_url", "http:/wahokamanwa7ed");
+
+                            new InsertTrackTask(getApplicationContext()) {
+                                @Override
+                                protected void onPostExecute(String s) {
+                                    try {
+                                        JSONObject response = new JSONObject(s);
+                                        String status = response.getString("status");
+                                        if (status.equals("fail")) {
+                                            Toast.makeText(ctx, "Failed to upload track! " + response.getString("error"), Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(ctx, "Success", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }.execute(json_track);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.execute(img);
                 break;
         }
     }
